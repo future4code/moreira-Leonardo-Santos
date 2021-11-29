34cde60980d4b0292bf323b6b8c40d7fcca06dee
Leo Christen Macedo dos Santos <leo.christenmacedo@gmail.com>
2021-11-29 12:01:07 -0300

    uploading in-class activities

---
+++ modulo1/aula-variaveis/assets/js/script.js
@@ -0,0 +1,42 @@
+/* variaveis que variam
+    let toVarianu = 0; <- declaração e atribuição;
+
+    toVarianu = 28; <- reatibuição;
+
+    toVarianu === 28; <- o valor atual da variavel;
+*/
+
+/* variaveis que não variam hehehe
+    const naoToVarianu = 265; <- declaração e atribuição;
+
+    naoToVarianu = 32; <- tentativa de reatribuição;
+
+    naoToVarianu === 256; (e um erro no console) <- ela não pode ser reatribuida;
+*/
+
+/* exercicio 1:
+
+const primeiroNome = 'Leo';
+const ultimoNome = 'Christen';
+const idade = 24;
+
+const statusTru = 'é estudante';
+const statusNoTru = 'n é estudante';
+
+console.log(`${primeiroNome} ${ultimoNome}, tem ${idade} anos, e ${statusTru}!`);
+*/
+
+/* exercicio 2:
+
+const nome = prompt('Qual é seu nome?');
+const idade = prompt('Qual é sua idade?');
+
+const idadeNum = Number(idade);
+
+console.log(typeof nome, typeof idadeNum);
+
+const nomeNum = Number(nome);
+const idadeStr = idadeNum.toString();
+
+console.log(typeof nomeNum, typeof idadeStr);
+*/
