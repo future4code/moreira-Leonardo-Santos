4fe7d42557bb56a042fa83f9e69339a2a7245d2d
Leo Christen Macedo dos Santos <leo.christenmacedo@gmail.com>
2022-01-15 09:05:01 -0300

    Applying some style to the app.js page

--- modulo2/projeto-react-whatslab/whatslab/src/App.js
+++ modulo2/projeto-react-whatslab/whatslab/src/App.js
@@ -3,31 +3,51 @@ import styled from 'styled-components';
 import ChatBubble from './components/ChatBubble/ChatBubble.js'
 
 const MainContainer = styled.div`
+  height: 80vh;
+  width: 340px;
+  margin: 60px auto 0 auto;
   display: flex;
   flex-direction: column;
-  justify-content: space-around;
+  justify-content: flex-end;
   align-items: center;
+  border: 1px solid black;
+  border-radius: 18px;
+  background: url('https://cdn.pixabay.com/photo/2019/03/03/20/23/flowers-4032775__340.png');
+  padding: 20px;
+`;
+
+const PageTitle = styled.h1`
+  position: fixed;
+  top: 0;
 `;
 
 class App extends React.Component{
 
   state = {
-    messages:
-    {
+    messages: [
+      {
         sendBy: "Leo",
         msg: "Lorem, ipsum dolor sit."
-    }
-    
+      },
+      {
+        sendBy: "Daru",
+        msg: "bla bla bla bla bla bla bla bla"
+      }
+    ]
   };
 
   render() {
-    
+
+    const chat = this.state.messages.map((message) => {
+      return <ChatBubble send={message.sendBy} msg={message.msg} />
+    });
+
     
 
     return (
       <MainContainer>
-        <h1>WhatsLab</h1>
-        <ChatBubble send={this.state.messages.sendBy} msg={this.state.messages.msg} />
+        <PageTitle>WhatsLab</PageTitle>
+        {chat}
       </MainContainer>
     );  
   };
