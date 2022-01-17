102afe7b5ffd3199616741579015a00cb12cc9f7
Leo Christen Macedo dos Santos <leo.christenmacedo@gmail.com>
2022-01-17 17:34:42 -0300

    Ënviando o que eu consegui por enquanto

--- modulo2/entrega-rend-condicional-form/rend-condicional-form/src/App.js
+++ modulo2/entrega-rend-condicional-form/rend-condicional-form/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import styled from "styled-components";
-import Step1 from './components/Step1';
-import Step2 from './components/Step2';
-import Step3 from './components/Step3';
-import Step4 from './components/Step4';
+import Step1 from './Components/Step1/Step1.js';
+import Step2 from './Components/Step2/Step2.js';
+import Step3 from './Components/Step3/Step3.js';
+import Step4 from './Components/Step4/Step4.js';
 
 const MainContainer = styled.div`
   text-align: center;
@@ -17,7 +17,7 @@ export default class App extends React.Component {
   render() {
     return (
       <MainContainer>
-
+        <Step3/>
       </MainContainer>
     );
   };
--- modulo2/entrega-rend-condicional-form/rend-condicional-form/src/components/Step2/Step2.js
+++ modulo2/entrega-rend-condicional-form/rend-condicional-form/src/components/Step2/Step2.js
@@ -1,10 +1,32 @@
 import React from "react";
 import styled from "styled-components";
 
+const Title = styled.h2`
+    text-transform: uppercase;
+`; 
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    height: 35vh;
+`;
+
+const Input = styled.input`
+    width: 10rem;
+    margin: 0 auto 1rem auto;
+`;
+
 export default class Step2 extends React.Component {
     render() {
         return (
-            <h1></h1>
+            <Form>
+                <Title>ETAPA 2 - INFORMAÇÕES DO ENSINO SUPERIOR</Title>
+                <label htmlFor="">5. Qual curso?</label>
+                <Input type="text" />
+                <label htmlFor="">6. Qual é unidade de ensino?</label>
+                <Input type="text" name="" id="" />
+            </Form>
         );
     };
 };
