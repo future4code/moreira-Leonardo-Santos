f4395e6ddff2fbfc406cf9ade0ac84388eb0df45
Leo Christen Macedo dos Santos <leo.christenmacedo@gmail.com>
2022-02-16 23:17:01 -0300

    Initial commit plus wireframe and project organization

---
+++ modulo3/projeto-labex/labex/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Sorry...</h1>
+      <h2>I don`t have any visible progress yet,
+        but please came back later 🫀</h2>
+    </>
+  );
+};
+
+export default App;
