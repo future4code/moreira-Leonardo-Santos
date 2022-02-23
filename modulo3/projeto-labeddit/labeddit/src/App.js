1b48df1799b9dd6e1a4b28722c41cb53f198a167
Leo Christen Macedo dos Santos <leo.christenmacedo@gmail.com>
2022-02-23 12:55:47 -0300

    Initial commit - wireframe, dir organization and surge link

---
+++ modulo3/projeto-labeddit/labeddit/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <h1>Ops, Sua página ainda não existe, mas garanto que estou trabalhando nela! 
+      </h1>
+      <h2>Fique a vontade para voltar mais tarde</h2>
+    </>
+  );
+};
+
+export default App;
