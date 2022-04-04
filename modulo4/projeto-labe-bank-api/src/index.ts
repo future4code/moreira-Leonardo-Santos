a0299d0c36f4f1a4874d7fd7189626fbefe055b7
Leo Christen Macedo dos Santos <leo.christenmacedo@gmail.com>
2022-04-04 10:24:07 -0300

    LabeBank - Basic requirements

--- modulo4/projeto-labe-bank-api/src/index.ts
+++ modulo4/projeto-labe-bank-api/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import { AddressInfo } from 'net';
+import { User } from './Types/Types';
+import { users } from './data/data';
 
 const app = express();
 
@@ -8,7 +10,39 @@ app.use(express.json());
 app.use(cors());
 
 app.post('/signup', (req, res) => {
+    let currentStatus = 400;
+    const {name, cpf, birth} = req.body;
 
+    try {
+        const birthNums: number[] = birth.split("/");
+        const currentYaer: number = new Date().getFullYear();
+        const age: number = currentYaer - birthNums[2]
+
+        if (age < 18) {
+            currentStatus = 401;
+            throw new Error("User doesn't have necessary age");
+        }
+
+        const newUser: User = {
+            id: Date.now().toString(),
+            name,
+            cpf,
+            birth,
+            balance: 0,
+            extract: []
+        };
+
+        users.push(newUser);
+
+        res.status(200).send('User created successfully');
+    }
+    catch (err: any) {
+        res.status(currentStatus).send(err.message)
+    };    
+});
+
+app.get('/users', (req, res) => {
+    res.status(200).send(users);
 });
 
 app.get('/account', (req, res) => {
