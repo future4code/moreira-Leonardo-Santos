82dca3d8f3d5827289866f227614c9300a624a5b
Leona Christen <leo.christenmacedo@gmail.com>
2022-04-28 21:19:21 -0300

    Enviando projeto Cookenu

---
+++ modulo4/projeto-cookenu-backend/src/data/endpoints/createUser.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+import connection from "../connection/connection";
+import { user } from "../Types/types";
+
+export default async function createUser(
+   req: Request,
+   res: Response
+): Promise<void> {
+   try {
+
+      const { name, nickname, email, address, cep, number, complement} = req.body
+
+      if (!name || !nickname || !email || !address || !cep || !number) {
+         res.statusCode = 422
+         throw "'name', 'nickname', 'email', 'address', 'cep', 'number' são obrigatórios"
+      }
+
+      const id: string = Date.now().toString()
+
+      const newUser: user = { id, name, nickname, email, address }
+
+      await connection('aula51_users').insert(newUser)
+
+      res.status(201).send("Usuário criado!")
+
+   } catch (error: any) {
+
+      if (typeof error === "string") {
+
+         res.send(error)
+      } else {
+         
+         console.log(error.sqlMessage || error.message);
+         res.status(500).send("Ops! Um erro inesperado ocorreu =/")
+      }
+
+   }
+}
