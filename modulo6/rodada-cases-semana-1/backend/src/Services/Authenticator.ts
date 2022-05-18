78ddfb03ae223c0234abba11cf27e8c8334d5fb1
Leona Christen <leo.christenmacedo@gmail.com>
2022-05-18 16:15:58 -0300

    Finished the backend tests and functionalities

--- modulo6/rodada-cases-semana-1/backend/src/Services/Authenticator.ts
+++ modulo6/rodada-cases-semana-1/backend/src/Services/Authenticator.ts
@@ -1,16 +1,16 @@
-import * as jwt from "jsonwebtoken";
-import { authenticationData } from "../types/authenticationData";
+// import * as jwt from "jsonwebtoken";
+// import { authenticationData } from "../types/authenticationData";
 
-export class Authenticator {
-  public generate = (input: authenticationData): string => {
-    const token = jwt.sign(input, String(process.env.JWT_KEY), {
-      expiresIn: process.env.EXPIRES_IN,
-    });
-    return token;
-  };
+// export class Authenticator {
+//   public generate = (input: authenticationData): string => {
+//     const token = jwt.sign(input, String(process.env.JWT_KEY), {
+//       expiresIn: process.env.EXPIRES_IN,
+//     });
+//     return token;
+//   };
 
-  public getTokenData = (token: string): authenticationData => {
-    const data = jwt.verify(token, String(process.env.JWT_KEY));
-    return data as authenticationData;
-  };
-}
+//   public getTokenData = (token: string): authenticationData => {
+//     const data = jwt.verify(token, String(process.env.JWT_KEY));
+//     return data as authenticationData;
+//   };
+// }
