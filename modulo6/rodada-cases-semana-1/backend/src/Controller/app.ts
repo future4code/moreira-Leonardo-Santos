fe84f2f67e9e465f397575a3a5a803f6ce599193
Leona Christen <leo.christenmacedo@gmail.com>
2022-05-19 11:44:26 -0300

    agora vai

--- modulo6/rodada-cases-semana-1/backend/src/Controller/app.ts
+++ modulo6/rodada-cases-semana-1/backend/src/Controller/app.ts
@@ -8,7 +8,7 @@ dotenv.config();
 export const app: Express = express();
 app.use(express.json());
 
-const server = app.listen(process.env.DB_PORT || 3003, () => {
+const server = app.listen(process.env.PORT || 3003, () => {
   if (server) {
     const address = server.address() as AddressInfo;
     console.log(`Server is running in http://localhost:${address.port}`);
